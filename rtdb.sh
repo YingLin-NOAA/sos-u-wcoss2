a11629bfc454772b0ede6a64e1051fff9c581f8b
YingLin-NOAA <ying.lin@noaa.gov>
2020-07-14 14:26:14 +0000

    update

--- rtdb.sh
+++ rtdb.sh
@@ -1,1 +1,13 @@
-nohup xterm -geometry 120x10+0+900 -sl 5000 -sb -bg orange -fg brown -e ssh venus 'export COMROOT=/gpfs/dell1/nco/ops/com; set term=xterm; export term; resize; cd /u/SDM/; ./rtdb \'
+#!/bin/sh
+# From Justin originally, 2020/07/14
+h1=`hostname | cut -c 1-1`
+if [ $h1 = m ]; then
+  host=mars
+elif [ $h1 = v ]; then
+  host=venus
+else
+  echo 'Host is neither Mars nor Venus, exit.' 
+fi
+nohup xterm -geometry 70x10+0+900 -sl 5000 -sb -bg orange -fg brown -e ssh $host 'export COMROOT=/gpfs/dell1/nco/ops/com; set term=xterm; export term; resize; cd /u/SDM/; ./rtdb \' &
+exit
+

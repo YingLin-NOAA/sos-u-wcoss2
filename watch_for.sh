6f1ca8083ec5e2f9660e34fc717408bf42963aac
YingLin-NOAA <ying.lin@noaa.gov>
2020-08-16 13:48:35 +0000

    update

---
+++ watch_for.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# watch for ensemble files to arrive at dcom.  Runs 
+# 
+# watch --interval=30 "ls -l /gpfs/dell1/nco/ops/dcom/prod/20200816/wgrbbul/ecmwf/DCE081600* | wc -l"
+# 
+# 
+if [ $# -gt 0 ]
+then
+  pattern=$1
+  if [ $# -eq 2 ] 
+  then
+    num_norm=$2
+  fi
+else
+  echo This script needs at least one argument.  Exit
+  exit
+fi
+
+if [ -z "$num_norm" ];
+then
+  watch --interval=30 "ls -l ${pattern}* | wc -l"
+else
+  watch --interval=30 "ls -l ${pattern}* | wc -l; echo out of $num_norm"
+fi
+
+exit
+
+

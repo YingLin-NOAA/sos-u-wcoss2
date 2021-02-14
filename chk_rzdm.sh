244c894c8ffd64bf154f93ba421ce5d2f171ba40
YingLin-NOAA <ying.lin@noaa.gov>
2021-02-14 04:21:14 +0000

    update

--- chk_rzdm.sh
+++ chk_rzdm.sh
@@ -3,10 +3,20 @@
 # check for connection time for each of the ncorzdm vm to see if one of them
 # is slow.  See Holly email 2020/12/31.
 #
+echo -e "\n Check CP RZDM:"
 for mem in 01 02 03 04 05 06 07 08
 do
   machine=vm-lnx-rzdm${mem}.ncep.noaa.gov
   time ssh -q -o BatchMode=yes nwprod@${machine}
-  echo above was for ssh to ${machine}
+  echo above is for ssh to ${machine}
 done
+
+echo -e "\n Check BLDR RZDM:"
+for mem in 01 02 03 04 05 06 07 08
+do
+  machine=vm-bldr-rzdm${mem}.ncep.noaa.gov
+  time ssh -q -o BatchMode=yes nwprod@${machine}
+  echo above is for ssh to ${machine}
+done
+
 exit

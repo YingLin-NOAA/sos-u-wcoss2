cc3b19674a801c0698e664c4d408e4b59eedc5f1
YingLin-NOAA <ying.lin@noaa.gov>
2021-02-24 11:08:41 +0000

    update

---
+++ rtdb_nox.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# like the rtdb.sh but w/o using xterm.
+
+export COMROOT=/gpfs/dell1/nco/ops/com; /u/SDM/rtdb
+#watch --interval 60 "export COMROOT=/gpfs/dell1/nco/ops/com; /u/SDM/rtdb"
+

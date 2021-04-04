0c9f6e150da1c74d1dcb368000d23fd1ffd795bd
Ying Lin <ying.lin@noaa.gov>
2021-04-04 14:58:11 +0000

    update

--- clock.sh
+++ clock.sh
@@ -2,7 +2,7 @@
 # little clock script originally from Justin
 while [ true ]
 do
- curr_time=`date -u "+%a %d %b %H:%M:%S"Z`
+ curr_time=`date -u "+%a %Y%m%d %H:%M:%S"Z`
  echo $curr_time
  sleep 1
  clear

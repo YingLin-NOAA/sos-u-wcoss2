9c9cc5b4c40f04aabc1c2ecee02ad975cd8ff429
Ying Lin <ying.lin@noaa.gov>
2022-01-12 21:23:58 +0000

    update

--- ring_bell.sh
+++ ring_bell.sh
@@ -1,16 +1,1 @@
-#!/bin/sh
-# This script sends plays the 'boing' sound on the home machine.  If run 
-# locally it looks for the IP address of your current log-in session to send
-# the ssh command to.  Can also be run remotely (with argument $i{home_ip}) 
-# from cpecflow1/cpecflow2 to sound the alert when running the ecflow log
-# monitoring script from these two machines, since cpecflow1[2] cannot ssh
-# through VPN to reach the home machine. 
-
-if [ $# -eq 0 ]
-then
-  home_ip=`who|grep "$USER"|grep -v localhost|tail -1|awk '{print $NF}'|sed 's/[()]//g'`
-else
-  home_ip=$1
-fi
-
-ssh ylin@${home_ip} "/usr/bin/afplay /Users/ylin/sounds/boing.au"
+echo -en "\007"
--- xterms.sh
+++ xterms.sh
@@ -1,20 +1,20 @@
 #!/bin/ksh
 #
+VERSION="20220111"
+
 # This script is a greatly simplified version of lt.sh.  It 
-# pops up a number of xterms. It takes 0, 1, or 3 arguments
+# pops up a number of xterms. It takes 0, 1, or 2 arguments
+# No RTDB pop-up for now
 
 #   No argument: 
-#     4 white for Dell
-#     1 lightgrey for Cray
-#     2 lightgoldenrod1 for Dev Dell
-#     1 small organge rtdb 
+#     4 white for prod
+#     2 lightgoldenrod1 for Dev 
 #   1 argument (n1): n1 white xterms
-#   3 argument (n1, n2, n3): n1/n2/n3 white/grey/gold xterms
+#   2 argument (n1, n2): n1/n2 white/gold xterms
 # RTDB: no special provision for restarting it in this script since it is easy
 #   to start it manually.  Note that RTDB should be restarted after 00Z during
 #   an evening/night shift.  
 
-VERSION="20220111"
 #
 
 #----------
@@ -30,21 +30,19 @@ then
   nwhite=4
   ngrey=1
   ngold=2
-  rtdb=Y
+  rtdb=N
 elif [ $# -eq 1 ]
 then
   nwhite=$1
-  ngrey=0
   ngold=0
   rtdb=N
-elif [ $# -eq 3 ]
+elif [ $# -eq 2 ]
 then
   nwhite=$1
-  ngrey=$2
-  ngold=$3
+  ngold=$2
   rtdb=N
 else
-  print "Number of arguments need to be 0, 1, or 3"
+  print "Number of arguments need to be 0, 1, or 2"
   exit
 fi
 
@@ -55,14 +53,6 @@ do
   sleep 1
   let n=n+1
 done 
-
-n=1
-while [ $n -le $ngrey ]
-do 
-  nohup  xterm -sl 5000 -sb  -bg ${X_BG2} -fa ${X_FT} &
-  sleep 1
-  let n=n+1
-done
 
 n=1
 while [ $n -le $ngold ]

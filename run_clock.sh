a8d5055f71680a51fecc156e117e661fbc2b09e9
Ying Lin <ying.lin@noaa.gov>
2022-08-15 08:09:04 +0000

    update

--- run_clock.sh
+++ run_clock.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-nohup xterm -geometry 70x10+0+900 -bg beige -e /u/ying.lin/sos/clock.sh & 
+# nohup xterm -geometry 70x10+0+900 -bg beige -e /u/ying.lin/sos/clock.sh & 
+xclock -digital -update 1 -face 'arial black-20:bold' -geom 400x50 &
 

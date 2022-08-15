0179c5182fe8ad437b69d884e7cf5557f896503f
Ying Lin <ying.lin@noaa.gov>
2022-08-15 09:51:37 +0000

    update

--- run_clock.sh
+++ run_clock.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 # nohup xterm -geometry 70x10+0+900 -bg beige -e /u/ying.lin/sos/clock.sh & 
-xclock -digital -update 1 -face 'arial black-20:bold' -geom 400x50 &
+if [ $# -eq 0 ];
+then 
+  xclock -digital -update 1 -face 'arial black-20:bold' -geom 400x50 &
+else
+  if [ $1 = 'big' ];
+  then
+    xclock -digital -update 1 -face 'arial black-40:bold' -geom 750x100 &
+  else
+    echo 'Unknow optional argument'
+  fi
+fi
 

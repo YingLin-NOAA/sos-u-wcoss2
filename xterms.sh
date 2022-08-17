f48b0abbac5867a15a4399b3bab21f91fecaa878
Ying Lin <ying.lin@noaa.gov>
2022-08-17 01:32:00 +0000

    update

--- xterms.sh
+++ xterms.sh
@@ -6,10 +6,11 @@ VERSION="20220111"
 # pops up a number of xterms. It takes 0, 1, or 2 arguments
 
 #   No argument: 
-#     4 LightCyan1 for prod
+#     5 LightCyan1 for prod
 #     1 Cornsilk1 for Dev 
+#     1 Seashell (beige) for edit text files (ng) while on loaner win GFE
 #   1 argument (n1): n1 xterms
-#   2 argument (n1, n2): n1/n2 xterms
+#   3 argument (n1, n2, n3): n1/n2/n3 xterms
 # RTDB: no special provision for restarting it in this script since it is easy
 #   to start it manually.  Note that RTDB should be restarted after 00Z during
 #   an evening/night shift.  
@@ -21,12 +22,14 @@ VERSION="20220111"
 #----------
 X_BG1="LightCyan1"
 X_BG2="Cornsilk1"
+X_BG3="Seashell"
 X_FT="'Monospace' -fs 14" 
 
 if [ $# -eq 0 ]
 then 
   n1=5
   n2=1
+  n3=1
   rtdb=Y
   clock=Y
 elif [ $# -eq 1 ]
@@ -41,8 +44,15 @@ then
   n2=$2
   rtdb=N
   clock=N
+elif [ $# -eq 3 ]
+then
+  n1=$1
+  n2=$2
+  n3=$3
+  rtdb=N
+  clock=N
 else
-  print "Number of arguments need to be 0, 1, or 2"
+  print "Number of arguments need to be between 0-3"
   exit
 fi
 
@@ -58,6 +68,14 @@ n=1
 while [ $n -le $n2 ]
 do 
   nohup  xterm -sl 5000 -sb  -bg ${X_BG2} -fa ${X_FT} &
+  sleep 1
+  let n=n+1
+done
+
+n=1
+while [ $n -le $n3 ]
+do 
+  nohup  xterm -sl 5000 -sb  -bg ${X_BG3} -fa ${X_FT} &
   sleep 1
   let n=n+1
 done

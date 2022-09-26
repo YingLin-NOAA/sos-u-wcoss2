c942a9015753bf98e85f1ecbdbe9065c77b653be
Ying Lin <ying.lin@noaa.gov>
2022-09-26 09:43:27 +0000

    update

--- chk_ecflog_active.sh
+++ chk_ecflog_active.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $# -eq 0 ]
 then
-  Echo This script needs an argument, c1/c2/d1/d2
+  echo This script needs an argument, c1/c2/d1/d2
   exit
 else
   arg1=$1
@@ -12,10 +12,13 @@ s1=`echo $arg1 | cut -c 2-2`
 ecf_server=${p1}ecflow0${s1}
 
 ecfdir=/lfs/h1/ops/prod/output/ecflow
-ecflog=$ecfdir/ecf.${ecf_server}.log
+ecflog=ecf.${ecf_server}.log
 
-tail -1000 $ecflog | grep active | grep -v ecflocal | grep -v backup | grep -v spa_on_call 
-tail -f $ecflog | grep active | grep -v ecflocal | grep -v backup | grep -v spa_on_call
+wrkdir=/lfs/h1/nco/stmp/ying.lin
+if [ ! -d $wrkdir ]; then mkdir $wrkdir; fi
+
+
+grep active $ecfdir/$ecflog | grep -v ecflocal | grep -v backup | grep -v spa_on_call > $wrkdir/$ecflog.active
 
 
 

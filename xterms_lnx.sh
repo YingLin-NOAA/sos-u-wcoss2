d57a9c06e7b6d332908b78c4129ee412cc236a46
Ying Lin <ying.lin@noaa.gov>
2025-09-15 18:40:59 +0000

    No longer have read permission to /u/nco.sdm/.  Remove the rtdb option.

--- xterms_lnx.sh
+++ xterms_lnx.sh
@@ -27,19 +27,16 @@ if [ $# -eq 0 ]
 then 
   n1=5
   n2=1
-  rtdb=Y
   clock=Y
 elif [ $# -eq 1 ]
 then
   n1=$1
   n2=0
-  rtdb=N
   clock=N
 elif [ $# -eq 2 ]
 then
   n1=$1
   n2=$2
-  rtdb=N
   clock=N
 else
   print "Number of arguments need to be 0, 1, or 2"
@@ -61,11 +58,6 @@ do
   sleep 1
   let n=n+1
 done
-
-if [ $rtdb = 'Y' ]
-then
-  ~/sos/rtdb.sh
-fi
 
 if [ $clock = 'Y' ]
 then
